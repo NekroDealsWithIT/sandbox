2984cda4c0242b607ddb77ea8486bf51e8b67fed
NekroDealsWithIT <38158326+NekroDealsWithIT@users.noreply.github.com>
2019-02-05 18:26:45 -0300

    Update eliteDangerousTools.js

--- static/js/eliteDangerousTools.js
+++ static/js/eliteDangerousTools.js
@@ -12,7 +12,8 @@ window.addEventListener("beforeunload", function(e){
 const URLJson="https://docs.google.com/spreadsheet/pub?key=1bMpMZnxUIcG7fgBcmzP8Np5v7fKT2VafDQy5Nv06_gs&single=true&gid=0&range=d3&output=csv";
 function getJson(){
 	$.ajax(URLJson).done(function(result){
-    	console.log(JSON.parse(result));
+    	
+	console.log(JSON.parse(strReplaceAll(result,"'",'"')));
     	alert(result);
 	});	
 }

1a2807a985fdd8c1d1ed491d183dd0bf0c2bcfd5
NekroDealsWithIT <38158326+NekroDealsWithIT@users.noreply.github.com>
2021-10-05 11:59:27 -0300

    Nav Bar FINAL

--- CAC/tpintegrador/js/custom.js
+++ CAC/tpintegrador/js/custom.js
@@ -1,4 +1,4 @@
 console.log("JS ok!");
-document.body.innerHTML=document.body.innerHTML+'<hr><h2>Assets</h2><img src="../assets/favicon/apple-touch-icon.png"><hr><h1>JS ok!</h1>';
+//document.body.innerHTML=document.body.innerHTML+'<hr><h2>Assets</h2><img src="../assets/favicon/apple-touch-icon.png"><hr><h1>JS ok!</h1>';
 	
 

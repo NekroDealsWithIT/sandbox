3ad8892c080dcbd312d0b2c9a9eb0df9592913fa
NekroDealsWithIT <38158326+NekroDealsWithIT@users.noreply.github.com>
2021-10-05 00:27:39 -0300

    Assets

--- CAC/tpintegrador/js/custom.js
+++ CAC/tpintegrador/js/custom.js
@@ -1,1 +1,4 @@
-console.log("archivo js ok!");
+console.log("JS ok!");
+document.body.innerHTML=document.body.innerHTML+'<hr><h2>Assets</h2><img src="../assets/favicon/apple-touch-icon.png"><hr><h1>JS ok!</h1>';
+	
+

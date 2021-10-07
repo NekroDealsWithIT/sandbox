6e80b9cb9d1e95753fa6bbb96ae9377dabb6fa06
NekroDealsWithIT <38158326+NekroDealsWithIT@users.noreply.github.com>
2021-10-07 12:07:35 -0300

    Form+Toggle IMG Ref

--- CAC/tpintegrador/js/custom.js
+++ CAC/tpintegrador/js/custom.js
@@ -1,4 +1,36 @@
 console.log("JS ok!");
 //document.body.innerHTML=document.body.innerHTML+'<hr><h2>Assets</h2><img src="../assets/favicon/apple-touch-icon.png"><hr><h1>JS ok!</h1>';
-	
+$("#oradorForm").submit(function(e) {
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+    var form = $(this);
+    console.log(form.serialize());
+    console.log(form);
 
+	const formData = new FormData(e.target);
+	const formProps = Object.fromEntries(formData);
+
+	console.log(formData);
+	console.log(formProps);	
+
+    alert('Hola: '+formProps.name+' '+formProps.surname+'.\nRecibimos tu solicitud y quedo registrada para hablar de:\n'+formProps.details);
+    /*var url = form.attr('action');*/
+    /*
+    $.ajax({
+           type: "POST",
+           url: url,
+           data: form.serialize(), // serializes the form's elements.
+           success: function(data)
+           {
+               alert(data); // show response from the php script.
+           }
+         });
+    */
+});
+
+function toggleImgRef(){
+	var element = document.getElementById("imgRef");
+	element.classList.toggle("hidden");
+
+	let accion=element.classList.contains('hidden')?'MOSTRAR':'OCULTAR';
+	toggleImgRefLink.innerHTML=accion+' IMG REFERENCIA <span class="sr-only">IMAGEN DE REFERENCIA</span>';
+}

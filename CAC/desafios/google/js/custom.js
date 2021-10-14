704eef88c3c0f315a880c95f6fe6bbfbbaa57334
NekroDealsWithIT <38158326+NekroDealsWithIT@users.noreply.github.com>
2021-10-14 01:29:24 -0300

    Google Argento Finish

--- CAC/desafios/google/js/custom.js
+++ CAC/desafios/google/js/custom.js
@@ -1,43 +1,38 @@
-const original = 'https://www.google.com/complete/search?q=a&cp=1&client=gws-wiz&xssi=t&hl=en-AR&authuser=0&psi=XUNnYbemCeq85OUPmfKjqAE.1634157405266&dpr=1';
-
-const url="https://www.google.com/complete/search?q=txt&cp=qty&client=gws-wiz&xssi=t&hl=en-AR&authuser=0&psi=XUNnYbemCeq85OUPmfKjqAE&dpr=1";
-function searchRecommendation(text){
-	let urlFinal = url.replace(/txt/g, text);
-	urlFinal = urlFinal.replace(/qty/g, text.length);
-
-	let recommendations=fetchUrl(urlFinal);
+searchInput.focus();
+function generatePrediction(){
+	let options;
+	thesaurusSpanish.forEach(t=>{
+		let option = document.createElement('option');
+		option.innerText=t;
+		option.value=t;
+		listChoices.appendChild(option);
+	})
 }
 
-function fetchUrl(url){
-	console.log(url);
-	postData(url, '')
-	  .then(data => {
-	    console.log(data); // JSON data parsed by `data.json()` call
-	  });
-	
-/*
-	fetch(url)
-	  .then(response => response.json())
-	  .then(data => console.log(data));
-	  .error(console.error())
-*/
+document.querySelector(".micSVG").addEventListener('click',function handler(event) {
+	alert('Tampoco la NASA.');
+});
 
-}
-
-	async function postData(url = '', data = {}) {
-	  // Default options are marked with *
-	  const response = await fetch(url, {
-	    method: 'GET', // *GET, POST, PUT, DELETE, etc.
-	    mode: 'no-cors', // no-cors, *cors, same-origin
-	    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-	    credentials: 'same-origin', // include, *same-origin, omit
-	    headers: {
-	      'Content-Type': 'application/json'
-	      // 'Content-Type': 'application/x-www-form-urlencoded',
-	    },
-	    redirect: 'follow', // manual, *follow, error
-	    referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-	    //body: JSON.stringify(data) // body data type must match "Content-Type" header
-	  });
-	  return response; // parses JSON response into native JavaScript objects
+function realizarBusqueda(){
+	let texto=searchInput.value;
+	if (texto.length>0){
+		alert('Ya te direcciono para buscar: '+texto);
+		window.location.href='https://www.google.com/search?q='+texto;
+	}else{
+		document.querySelector('.JAJAJJAJAJAJAJAJA').classList.add('hidden');
+		document.querySelector('.facepalm').classList.remove('hidden');
+		searchInput.focus();
 	}
+}
+
+function mostrarPalabras(){
+	divPalabras.classList.toggle('hidden');
+	let words;
+	if(document.querySelector('#predictorWords').innerHTML==''){
+		thesaurusSpanish.forEach(t=>{
+			words+=t+' ,';
+		})
+		document.querySelector('#predictorWords').innerHTML="<h1>Puedo predecir "+thesaurusSpanish.length+1+" palabras en espa&ntilde;ol</h1>";
+		document.querySelector('#predictorWords').innerHTML+=words;
+	}
+}
--- CAC/desafios/google/js/extractor.js
+++ CAC/desafios/google/js/extractor.js
@@ -1,1 +1,10 @@
 var palabras="";document.querySelectorAll("h4").forEach(e=>{palabras+='"'+e.innerText+'",';});palabras;
+
+/*
+var script = document.createElement('script');
+script.onload = function() {
+  alert("Script loaded and ready");
+};
+script.src = "http://whatever.com/the/script.js";
+document.getElementsByTagName('head')[0].appendChild(script);
+*/
